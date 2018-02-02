749a9cc4d1b2ec7250957d1a8a45a6db793b4562
Danny <daniel@Papa-Smurf.local>
2018-02-01 18:53:17 -0800

    added UIView
    - adds blue dots to the UI

--- PootCal/PootCal/CAL/ViewController.swift
+++ PootCal/PootCal/CAL/ViewController.swift
@@ -26,19 +26,7 @@ class ViewController: UIViewController {
 
 }
 
-extension ViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
-    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
-        
-    }
-    
-    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
-        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "DateCell", for: indexPath) as! DateCell
-        cell.dateLabel.text = cellState.text
-
-        self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
-        return cell
-    }
-
+extension ViewController: JTAppleCalendarViewDataSource {
     
     func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
         
@@ -52,5 +40,19 @@ extension ViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDele
         let parameters = ConfigurationParameters(startDate: start, endDate: end)
         return parameters
     }
+}
+
+extension ViewController: JTAppleCalendarViewDelegate {
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {}
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "DateCell", for: indexPath) as! DateCell
+        cell.dateLabel.text = cellState.text
+        
+        self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
+        return cell
+    }
+
+    
 }
 
