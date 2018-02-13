50c7e636af44f1eaab5d59fc91b8f93ec271f3a0
Danny <uhohitsdanny@gmail.com>
2018-02-12 17:08:21 -0800

    Fixed dates in n out color problem
    - fixed the bug where dates of the current month is grayed after scrolling back

--- PootCal/PootCal/CAL/View Controller/Cal_VC.swift
+++ PootCal/PootCal/CAL/View Controller/Cal_VC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import JTAppleCalendar
 
-class ViewController: UIViewController {
+class Cal_VC: UIViewController {
     @IBOutlet weak var calendarView: JTAppleCalendarView!
     @IBOutlet weak var monthLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
@@ -19,54 +19,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         setupCalendar()
-    }
-
-    func setupCalendar() {
-        calendarView.minimumLineSpacing = 0
-        calendarView.minimumInteritemSpacing = 0
-        calendarView.allowsMultipleSelection = true
-        
-        //set up labels
-        calendarView.visibleDates { (visibleDates) in
-            self.setupMonthAndYear(from: visibleDates)
-        }
-    }
-    
-    func setupMonthAndYear(from visibleDates: DateSegmentInfo){
-        let date = visibleDates.monthDates.first!.date
-        //format year and month
-        self.formatter.dateFormat = "yyyy"
-        self.yearLabel.text = self.formatter.string(from: date)
-        
-        self.formatter.dateFormat = "MMMM"
-        self.monthLabel.text = self.formatter.string(from: date)
-    }
-    
-    func setupCellNotations(cell: JTAppleCell?, cellState: CellState) {
-        guard let validCell = cell as? DateCell else {  return  }
-        if cellState.isSelected {
-            validCell.selectedView.isHidden = false
-        } else {
-            validCell.selectedView.isHidden = true
-        }
-    }
-    
-    func handleCellTextColor(cell: JTAppleCell?, cellState: CellState) {
-        guard let validCell = cell as? DateCell else {  return  }
-        if cellState.dateBelongsTo != .thisMonth {
-            validCell.dateLabel.textColor = UIColor.lightGray
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
-extension ViewController: JTAppleCalendarViewDataSource {
+extension Cal_VC: JTAppleCalendarViewDataSource {
     
     func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
         
@@ -82,8 +39,10 @@ extension ViewController: JTAppleCalendarViewDataSource {
     }
 }
 
-extension ViewController: JTAppleCalendarViewDelegate {
-    //Display the cell
+extension Cal_VC: JTAppleCalendarViewDelegate {
+    //  *   Displays the cell    *
+    
+    //Protocols for JTAppleCalendar
     func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
     }
     
@@ -113,5 +72,49 @@ extension ViewController: JTAppleCalendarViewDelegate {
         setupMonthAndYear(from: visibleDates)
     }
 }
+
+extension Cal_VC {
+    
+    //  *   Setup Functions *
+    
+    func setupCalendar() {
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+        calendarView.allowsMultipleSelection = true
+        
+        //set up labels
+        calendarView.visibleDates { (visibleDates) in
+            self.setupMonthAndYear(from: visibleDates)
+        }
+    }
+    func setupMonthAndYear(from visibleDates: DateSegmentInfo){
+        let date = visibleDates.monthDates.first!.date
+        //format year and month
+        self.formatter.dateFormat = "yyyy"
+        self.yearLabel.text = self.formatter.string(from: date)
+        
+        self.formatter.dateFormat = "MMMM"
+        self.monthLabel.text = self.formatter.string(from: date)
+    }
+    
+    func setupCellNotations(cell: JTAppleCell?, cellState: CellState) {
+        guard let validCell = cell as? DateCell else {  return  }
+        if cellState.isSelected {
+            validCell.selectedView.isHidden = false
+        } else {
+            validCell.selectedView.isHidden = true
+        }
+    }
+    
+    func handleCellTextColor(cell: JTAppleCell?, cellState: CellState) {
+        guard let validCell = cell as? DateCell else {  return  }
+        if cellState.dateBelongsTo != .thisMonth {
+            validCell.dateLabel.textColor = UIColor.lightGray
+        } else {
+            validCell.dateLabel.textColor = UIColor.black
+        }
+    }
+}
+
 
 
